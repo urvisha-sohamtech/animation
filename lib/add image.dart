36669fa0a80c2a264294369b1e3accb26d8b571d
DELL <urvisha.sohamtech@gmail.com>
2023-04-29 03:12:50 -0700

    test

--- lib/add image.dart
+++ lib/add image.dart
@@ -11,19 +11,15 @@ class _HomePageState extends State<HomePage> {
   final picker = ImagePicker();
   List imageList = [];
   void selectImage() async{
-    final selected =
-    await picker.pickImage(source: ImageSource.gallery);
-    if(selected!.path.isNotEmpty){
+    final selected = await picker.pickImage(source: ImageSource.gallery);
       setState(() {
         imageList.add(selected);
       });
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.white24,
       appBar:AppBar(title: Text('image'),
         backgroundColor: Colors.black54,
         centerTitle: true,
@@ -38,18 +34,21 @@ class _HomePageState extends State<HomePage> {
               child: Text('Add image',style: TextStyle(color: Colors.black),),
             ),
            Expanded(
-               child:AlignedGridView.count(
-                   crossAxisCount:3,
-                   itemCount: imageList.length,
-                   itemBuilder: (context,index){
-                     return Padding(
-                       padding: EdgeInsets.all(2.0),
-                       child: Image.file(
-                           File(imageList[index].path),
-                         fit: BoxFit.cover,
-                       ),
-                     );
-                   }
+               child:Padding(
+                 padding:  EdgeInsets.all(8.0),
+                 child: AlignedGridView.count(
+                     crossAxisCount:2,
+                     itemCount: imageList.length,
+                     itemBuilder: (context,index){
+                       return Padding(
+                         padding: EdgeInsets.all(2.0),
+                         child: Image.file(
+                             File(imageList[index].path),
+                           fit: BoxFit.cover,
+                         ),
+                       );
+                     }
+                 ),
                )
            )
           ],
